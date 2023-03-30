ccadbf16626c3c7ef075c18029043fecdc72fcef
cony0907 <syk920907@gmail.com>
2023-03-30 17:44:23 +0900

    myport

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,81 @@
+import TeslaLogo from "./images/tesla.png";
+
 function App() {
   return (
-    <div>
-      <h1>hello, react</h1>
-      <h2>my name is cony</h2>
-      <h3>I like coding</h3>
+    <div className="bg-red-100 min-h-screen">
+      <header className="bg-blue-100 h-12 md:h-20">
+        <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
+          <div>
+            <img className="w-20" src={TeslaLogo} alt="tesla" />
+          </div>
+          <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
+            <li className="bg-indigo-300 btn-style">Introduce</li>
+            <li className="bg-pink-300 btn-style">Portfolio</li>
+            <li className="bg-green-300 btn-style">Contact Me</li>
+          </ul>
+        </div>
+      </header>
+      <main>
+        <div className="bg-green-100 maw-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          소개 내용
+        </div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
+        <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          내 연락처
+        </div>
+      </main>
+      <footer className="mx-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+        2023-03-30 by cony
+      </footer>
     </div>
   );
 }
